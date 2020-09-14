eb30fc74da4f16cee8a1aa8415726c4f030e9a44
vinodh-dev <nativehub39@gmail.com>
2020-09-14 17:04:43 +0530

    wfwfw

--- Screens/Api/Api.js
+++ Screens/Api/Api.js
@@ -3,5 +3,5 @@ export const  LOGIN_API ="http://qrweb.co/ecommerce/public/api/driverlogin"
 export const  ASSIGNED_ORDER ="http://qrweb.co/ecommerce/public/api/assignedorder"
 export const  CHANGE_PASSWORD ="http://qrweb.co/ecommerce/public/api/driverchangepassword"
 export const  HISTORY_API ="http://qrweb.co/ecommerce/public/api/driver_orderhistory"
-export const  DELIVERY_STATUS="http://qrweb.co/ecommerce/public/api/updatedeliverystatus"
+export const  DELIVERY_STATUS="http://qrweb.co/ecommerce/public/api/updatedeliverystatus"  
 
